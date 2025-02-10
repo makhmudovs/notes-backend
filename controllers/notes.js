ca5b45869dd3a140f1230deaafadb65b07cfbfdc
makhmudovs <makhmudovs50@gmail.com>
2025-02-10 15:37:06 +0800

    tests added

--- controllers/notes.js
+++ controllers/notes.js
@@ -2,28 +2,28 @@ const notesRouter = require('express').Router();
 const Note = require('../models/note');
 
 //get all the notes
-notesRouter.get('/', (req, res,next) => {
-    Note.find({}).then((notes) => {
-        res.json(notes);
-    }).catch(err => next(err));
+notesRouter.get('/',async (req, res) => {
+    const notes = await Note.find({});
+    res.json(notes);
 });
 
 // get a specific note by an id
-notesRouter.get('/:id', (req, res, next) => {
-    Note.findById(req.params.id)
-        .then((note) => {
-            if (note) {
-                res.json(note);
-            } else {
-                res.status(404).end();
-            }
-        })
-        .catch((err) => next(err));
+notesRouter.get('/:id', async (req, res, next) => {
+    try {
+        const note = await Note.findById(req.params.id);
+        if (note) {
+            res.json(note);
+        } else {
+            res.status(404).end();
+        }
+    } catch (exception) {
+        next(exception);
+    }
 });
 
 
 //post new note
-notesRouter.post('/', (req, res, next) => {
+notesRouter.post('/', async (req, res, next) => {
     const body = req.body;
 
     const note = new Note({
@@ -31,20 +31,23 @@ notesRouter.post('/', (req, res, next) => {
         important: body.important || false,
     });
 
-    note.save()
-        .then((savedNote) => {
-            res.json(savedNote);
-        }).catch((err) => next(err));
+    try {
+        const savedNote = await note.save();
+        res.status(201).json(savedNote);
+    } catch(exception) {
+        next(exception);
+    }
 });
 
 
 //delete a note by and id
-notesRouter.delete('/:id', (req, res, next) => {
-    Note.findByIdAndDelete(req.params.id)
-        .then(() => {
-            res.status(204).end();
-        })
-        .catch((err) => next(err));
+notesRouter.delete('/:id', async (req, res, next) => {
+    try {
+        await Note.findByIdAndDelete(req.params.id);
+        res.status(204).end();
+    } catch (exception) {
+        next(exception);
+    }
 });
 
 
